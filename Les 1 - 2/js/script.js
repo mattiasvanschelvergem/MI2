e47968e0df77dbc624f30b74cb02247e21a1b732
mattiasvans <43760621+mattiasvanschelvergem@users.noreply.github.com>
2019-05-11 21:41:02 +0200

    Add files via upload

---
+++ Les 1 - 2/js/script.js
@@ -0,0 +1,33 @@
+
+
+if ('click', '#normaal') {
+	let function1 = function('van.jpg') {
+	filter:normal(100%);	
+}
+}
+
+if ('click', '#grijs') {
+	let function2 = function('van.jpg') {
+	filter:grayscale(100%);	
+}
+}
+
+if ('click', '#sepia') {
+	let function3 = function('van.jpg') {
+	filter:sepia(100%);
+}
+}
+
+if ('click', '#hue') {
+	let function4 = function('van.jpg') {
+	filter:hue-rotate(90deg);
+}
+
+if ('click', '#blur') {
+	let function5 = function('van.jpg') {
+	-webkit-filter: blur(5px);
+	filter: blur(5px);
+}
+}
+
+
